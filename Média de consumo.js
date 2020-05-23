b033cc300df40d8ec55d1d6ac2fb5ffcd7f1a2ed
Itamar Ribeiro <49563954+IASR19@users.noreply.github.com>
2020-05-23 14:57:10 -0300

    Add files via upload

---
+++ Média de consumo.js
@@ -0,0 +1,14 @@
+// Giovanna Moreira Borsato Turma 22
+// Programa 1
+
+var v = [], aux=0, media, i; // vetor e variáveis
+
+for(i = 1; i<=6; i++) // laço de repetição
+{
+  v[i] = prompt ("Digite o " + i + "o valor: ");
+  aux += parseInt(v[i]); // variável para armazenar somas das potências
+}
+
+media = parseFloat(aux/6); // varável que faz o cálculo da média
+
+console.log("\nO valor médio de consumo é de: " + media + " KWh");
